df3b08a646f17c9b87175c8828b883388f639a95
LOKEYYYYY <loke.yingxin@gmail.com>
2026-04-09 16:54:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -229,5 +229,8 @@ def home():
 # ==============================
 # RUN SERVER
 # ==============================
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Railway gives this
+    app.run(host="0.0.0.0", port=port)
